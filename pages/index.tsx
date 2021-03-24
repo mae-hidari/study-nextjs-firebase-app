9a39f4b0e69e28e5ed6cff05f6377bc55236f9bd
Kento Kuga <kento.kuga.hidari@gmail.com>
2021-03-24 23:46:29 +0900

    refactor：simple toppage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,84 +1,20 @@
-// import Head from "next/head";
-// import Link from "next/link";
-// import { useAuthenticate } from "../hooks/authentication";
-// import styles from "../styles/Home.module.css";
+import Head from "next/head";
+import { useAuthenticate } from "../hooks/authentication";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  // const { user } = useAuthenticate();
+  const { user } = useAuthenticate();
 
-  // return (
-  //   <div className={styles.container}>
-  //     <Head>
-  //       <title>Create Next App</title>
-  //       <link rel="icon" href="/favicon.ico" />
-  //     </Head>
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-  //     <main className={styles.main}>
-  //       <h1 className={styles.title}>
-  //         Welcome to <a href="https://nextjs.org">Next.js!</a>
-  //       </h1>
-
-  //       <p>{user?.uid || "未ログイン"}</p>
-
-  //       <Link href="/page2">
-  //         <a>Go to page2</a>
-  //       </Link>
-
-  //       <Link href={`/users/aey8C3sD2pMNeT9l0KkmAhhVYXH2`}>
-  //         <a>Go to users</a>
-  //       </Link>
-
-  //       <Link href={`/questions/received`}>
-  //         <a>Go to received questions</a>
-  //       </Link>
-
-  //       <p className={styles.description}>
-  //         Get started by editing{" "}
-  //         <code className={styles.code}>pages/index.js</code>
-  //       </p>
-
-  //       <div className={styles.grid}>
-  //         <a href="https://nextjs.org/docs" className={styles.card}>
-  //           <h3>Documentation &rarr;</h3>
-  //           <p>Find in-depth information about Next.js features and API.</p>
-  //         </a>
-
-  //         <a href="https://nextjs.org/learn" className={styles.card}>
-  //           <h3>Learn &rarr;</h3>
-  //           <p>Learn about Next.js in an interactive course with quizzes!</p>
-  //         </a>
-
-  //         <a
-  //           href="https://github.com/vercel/next.js/tree/master/examples"
-  //           className={styles.card}
-  //         >
-  //           <h3>Examples &rarr;</h3>
-  //           <p>Discover and deploy boilerplate example Next.js projects.</p>
-  //         </a>
-
-  //         <a
-  //           href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-  //           className={styles.card}
-  //         >
-  //           <h3>Deploy &rarr;</h3>
-  //           <p>
-  //             Instantly deploy your Next.js site to a public URL with Vercel.
-  //           </p>
-  //         </a>
-  //       </div>
-  //     </main>
-
-  //     <footer className={styles.footer}>
-  //       <a
-  //         href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Powered by{" "}
-  //         <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-  //       </a>
-  //     </footer>
-  //   </div>
-  // );
-  return <div>ようこそ</div>;
+      <main className={styles.main}>
+        <p>{user?.uid || "未ログイン"}</p>
+      </main>
+    </div>
+  );
 }
