da89867e51769a8cb8d4df0fa0d327ac433639fd
Kento Kuga <kento.kuga.hidari@gmail.com>
2021-03-24 23:21:01 +0900

    fix：Firebase is not initialized when deployed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import Head from "next/head";
-import Link from "next/link";
-import { useAuthenticate } from "../hooks/authentication";
-import styles from "../styles/Home.module.css";
+// import Head from "next/head";
+// import Link from "next/link";
+// import { useAuthenticate } from "../hooks/authentication";
+// import styles from "../styles/Home.module.css";
 
 export default function Home() {
   // const { user } = useAuthenticate();
