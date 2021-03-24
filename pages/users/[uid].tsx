041ad6be58f288bba3c09ce23569acf651cbc9b4
Kento Kuga <kento.kuga.hidari@gmail.com>
2021-03-24 23:24:06 +0900

    Revert "fix：Firebase is not initialized when deployed"
    This reverts commit d5a3c71a4c572b8bcb51ff9f3ae7c3303747fc51.

---
+++ pages/users/[uid].tsx
@@ -0,0 +1,120 @@
+import { useRouter } from "next/router";
+import { FormEvent, useState, useEffect } from "react";
+import { User } from "../../model/User";
+import firebase from "firebase/app";
+import Layout from "../../components/Layout";
+import { toast } from "react-toastify";
+
+type Query = {
+  uid: string;
+};
+
+export default function UserShow() {
+  const [user, setUser] = useState<User>(null);
+  const [body, setBody] = useState("");
+  const [isSending, setIsSending] = useState(false);
+
+  const router = useRouter();
+  const query = router.query as Query;
+  const currentUser = firebase.auth().currentUser;
+
+  useEffect(() => {
+    if (query.uid === undefined) {
+      return;
+    }
+
+    const loadUser = async () => {
+      const doc = await firebase
+        .firestore()
+        .collection("users")
+        .doc(query.uid)
+        .get();
+
+      if (!doc.exists) {
+        return;
+      }
+
+      const getUser = doc.data() as User;
+      getUser.uid = doc.id;
+      setUser(getUser);
+    };
+    loadUser();
+  }, [query.uid]);
+
+  const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!body) {
+      return;
+    }
+
+    setIsSending(true);
+
+    await firebase.firestore().collection("questions").add({
+      senderUid: firebase.auth().currentUser.uid,
+      receiverUid: user.uid,
+      body,
+      isReplied: false,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+
+    setIsSending(false);
+
+    setBody("");
+
+    toast.success("質問を送信しました。", {
+      position: "bottom-left",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  };
+
+  return (
+    <Layout>
+      {user && currentUser && (
+        <>
+          <div className="text-center">
+            <h1 className="h4">{user.name}さんのページ</h1>
+            <div className="m-5">{user.name}さんに質問しよう!</div>
+            <div className="row justify-content-center mb-3">
+              <div className="col-12 col-md-6">
+                {user.uid === currentUser.uid ? (
+                  <div>自分には送信できません。</div>
+                ) : (
+                  <form onSubmit={onSubmit}>
+                    <textarea
+                      className="form-control"
+                      placeholder="おげんきですか？"
+                      rows={6}
+                      required
+                      value={body}
+                      onChange={(e) => setBody(e.target.value)}
+                    ></textarea>
+                    <div className="m-3">
+                      {isSending ? (
+                        <div
+                          className="spinner-border text-secondary"
+                          role="status"
+                        >
+                          <span className="visually-hidden">Loading...</span>
+                        </div>
+                      ) : (
+                        <button type="submit" className="btn btn-primary">
+                          質問を送信する
+                        </button>
+                      )}
+                    </div>
+                  </form>
+                )}
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </Layout>
+  );
+}
